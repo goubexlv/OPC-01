bf4994af034c9ed5bf2921bc59592ef5578033e1
goubexlv <93274828+goubexlv@users.noreply.github.com>
2024-09-12 23:52:40 +0200

    fin projet
    La fin du projet

--- ENEO/src/Main.kt
+++ ENEO/src/Main.kt
@@ -9,66 +9,37 @@ fun List<Double>.moyenneConsommations(): Double {
 }
 
 fun main() {
-    var nomClient : String?
-    var numCompteur :Int?
-    var consommation : Double
-    val tauxConsommation : Int = 50
-
-    var choix1 = true
+    val listeClients = mutableListOf<Clients>()
+    var choix1 :Int = 2
     var choix :Int
 
     println("###########################################")
     println("Bienvenue à ENEO !")
     println("###########################################")
 
-    val listeClients = mutableListOf(
-        Clients("junior", 123456, 40.0)
-       )
-
-
-    if(verifi_consommation(listeClients[0].consommation)) {
-        listeClients[0].netPayer = tauxConsommation*listeClients[0].consommation
-    }
-    var listeConsommationClient = listOf(listeClients[0].netPayer)
-    println(listeConsommationClient.moyenneConsommations())
-
     do{
+
+
         println("1- Ajouter un client\n" +
                 "2- Calculer les factures avec précision \n" +
                 "3- Détecter les anomalies de consommation\n" +
                 "4- Fournir un résumé global de la consommation des clients")
 
-        choix = 1
+        println("Entrez votre choix :")
+        val console  = Scanner(System.`in`)
+        choix = console.nextInt()
         when(choix){
             1 -> listeClients.add(CreeClient())
-            2 -> {
-                println("###########################################")
-                println("Calculer les factures avec précision")
-                println("###########################################")
-                for(i in 0..listeClients.size){
-                    if(verifi_consommation(listeClients[i].consommation)) {
-                        listeClients[i].netPayer = tauxConsommation*listeClients[i].consommation
-                    }
-                }
-            }
+            2 -> Calcul_facturation(listeClients)
+            3 -> afficher_anomali(listeClients)
+            4 -> resume_global(listeClients)
 
-            3 -> {
-                println("###########################################")
-                println("Détecter les anomalies de consommation")
-                println("###########################################")
-                for(i in 0..listeClients.size){
-                    println("Client $i\n" +
-                            "Nom : ${listeClients[i].netPayer}\n" +
-                            "facturation : ${listeClients[i].netPayer} XAF\n" +
-                            "Anormali : ${Detection_anomalie(listeClients[i].netPayer)}")
-                    println("///////////////////////////////////////////")
-
-                }
-            }
             else -> println("Choix non disponible")
         }
-
-    }while(choix1 == true)
+        println("Voulez vous quitter le programme : \n" +
+                "1. oui et 2. non")
+        choix1 = console.nextInt()
+    }while(choix1 == 2)
 
 }
 
@@ -103,33 +74,76 @@ fun CreeClient() : Clients {
     val console  = Scanner(System.`in`)
     println("Entrez le nom client :")
     nomClient = console.nextLine()
-    println("Entrez le nom client :")
+    println("Entrez le numéro de compteur :")
     numCompteur = console.nextInt()
-    println("Entrez le nom client :")
+    println("Entrez la consommation :")
     consommation = console.nextDouble()
 
     return Clients(nomClient, numCompteur, consommation)
 }
 
-fun Detection_anomalie(consommation : Double){
+
+
+fun Calcul_facturation(listClients: List<Clients>) {
+    val tauxConsommation : Int = 50
+    println("###########################################")
+    println("Calculer les factures avec précision")
+    println("###########################################")
+    for(i in 0..listClients.size-1){
+        if(verifi_consommation(listClients[i].consommation) == true) {
+            listClients[i].netPayer = tauxConsommation*listClients[i].consommation
+            println("Client $i\n" +
+                    "Nom : ${listClients[i].nomClient}\n" +
+                    "Consommation : ${listClients[i].consommation} \n" +
+                    "Facturation : ${listClients[i].netPayer} XAF")
+            println("///////////////////////////////////////////")
+
+        }
+    }
+}
+
+fun afficher_anomali(listClients: List<Clients>){
+    println("###########################################")
+    println("Détecter les anomalies de consommation")
+    println("###########################################")
+    for(i in 0..listClients.size-1){
+        println("Client $i\n" +
+                "Nom : ${listClients[i].nomClient}\n" +
+                "Consommation : ${listClients[i].consommation} XAF\n" +
+                "Anormali : ${Detection_anomalie(listClients[i].consommation)}")
+        println("///////////////////////////////////////////")
+
+    }
+}
+
+fun Detection_anomalie(consommation : Double): Any {
 
     return try {
-        if (consommation > 50) {
+        if (consommation > 5000) {
             throw consommationanormalexeption("La consommation $consommation est anormale.")
         }
-        println("La consommation $consommation est valide.")
+            return "La consommation $consommation n'est pas anormale."
     } catch (e: consommationanormalexeption) {
-        println("Erreur : ${e.message}")
+        return "Erreur : ${e.message}"
     } finally {
-        println("Vérification terminée.")
+
     }
 
 }
 
-fun afficherClient(listClients: List<Any>){
-    println("Liste client")
-    for(i in 1..listClients.size){
-        println("$i  - ${listClients[0] }")
-    }
+fun resume_global(listClients: List<Clients>){
+    println("###########################################")
+    println("Fournir un résumé global de la consommation des clients")
+    println("###########################################")
+    val listeConsommationClient = mutableListOf<Double>()
+    for (i in 0..listClients.size-1)
+        listeConsommationClient.add(listClients[i].consommation)
+
+    println("Consommation total des clients : ${listeConsommationClient.sum()} \n" +
+            "Moyenne de consommation : ${listeConsommationClient.moyenneConsommations()}\n" )
+
 }
+
+
+
 
